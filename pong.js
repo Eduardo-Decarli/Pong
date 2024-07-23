dbbd9941470abdcaeec736f896f864fc6793c8f8
Eduardo Decarli <decarliedu@gmail.com>
2024-07-22 23:36:01 -0300

    Lógica colisão e pontuação + desenho dos objetos

--- pong.js
+++ pong.js
@@ -34,12 +34,99 @@ const computer = {
 
 //Desenho do Player
 function drawRect(x, y, w, h, color){
+    //método define a cor utilizada na função drawRect
     context.fillStyle = color;
+    //método utilizado para desenhar retângulos
     context.fillRect(x,y,w,h);
 }
 
-//desenho da Bola
+//Desenho da Bola
 function drawCircle(x, y, r, color){
     context.fillStyle = color;
-
+    context.beginPath();
+    //Método arc() Cria a curvatura da bola
+    context.arc(x, y, r, 0, 2*Math.PI);
+    context.closePath();
+    context.fill();
 }
+
+//Desenho do Placar
+function drawText(text, x, y, color){
+    context.fillStyle = color;
+    context.font = "45px fantasy";
+    context.fillText(text, x, y);
+}
+
+//Renderização dos Elementos
+function render(){
+    //Limpa o canvas
+    drawRect(0, 0, canvas.width, canvas.height, 'BLACK');
+
+    //Desenho do Placar
+    drawText(user.score, canvas.width / 4, canvas.height / 5, 'WHITE')
+    drawText(computer.score, 3*canvas.width/4, canvas.height/5, 'WHITE')
+
+    //Desenho da Bola
+    drawCircle(ball.x, ball.y, ball.radius, ball.color);
+
+    //Desenho das raquetes
+    drawRect(user.x, user.y, user.width, user.height, user.color);
+    drawRect(computer.x, computer.y, computer.width, computer.height, computer.color);
+}
+
+//Lógica do jogo
+function update(){
+    //movimento da bola
+    ball.x += ball.velocityX;
+    ball.y += ball.VelocityY;
+
+    //detecção de colição com a corda superior e inferior
+    if(ball.y + ball.radius > canvas.height || ball.y - ball.canvas < 0){
+        ball.VelocityY = -ball.VelocityY;
+    }
+
+    //detecta colição com a borda esquerda e direita
+    if(ball.x + ball.radius > canvas.width){
+        user.score++;
+        resetBall();
+    } else if(ball.x - ball.radius < 0){
+        computer.score++;
+        resetBall();
+    }
+
+    //detecção com a raquete
+    let player = (ball.x < canvas.width/2) ? user : computer
+
+    if(colision(ball, player)){
+        ball.velocityX = -ball.velocityX;
+    }
+};
+
+//Sistema de Colisão
+function colision(b, p) {
+    p.top = p.y;
+    p.bottom = p.y + p.height;
+    p.left = p.x;
+    p.right = p.x + p.width;
+
+    b.top = b.y - b.radius;
+    b.bottom = b.y + b.radius;
+    b.left = b.x - b.radius;
+    b.right = b.x + b.radius;
+    return b.right > p.left && b.bottom > p.top && b.left < p.right && b.top < p.botoom;
+}
+
+function resetBall(){
+    ball.x = canvas.width/2
+    ball.y = canvas.hight/2
+    ball.velocityX = -ball.velocityX
+    ball.speed = 7;
+}
+
+function game(){
+    render();
+}
+
+//loop do jogo
+const framePerSecond = 50;
+setInterval(game, 1000/framePerSecond);
