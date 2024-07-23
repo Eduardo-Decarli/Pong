f2abc6f330bce5b4031326c5eea7d1777504c0da
Eduardo Decarli <decarliedu@gmail.com>
2024-07-23 11:06:47 -0300

    Finalização do Projeto

--- pong.js
+++ pong.js
@@ -7,29 +7,31 @@ const ball = {
     y: canvas.height / 2,
     radius: 10,
     velocityX: 5,
-    VelocityY: 5,
+    velocityY: 5,
     speed: 7,
     color: 'WHITE'
 };
 
 //criação da raquete
 const user = {
-    x: 0,
+    x: 10,
     y: (canvas.height -100) / 2,
     width: 10,
     height: 100,
     color: 'WHITE',
-    score: 0
+    score: 0,
+    dy: 0
 };
 
 //Boot do player2
 const computer = {
-    x: canvas.width -10,
+    x: canvas.width - 20,
     y: (canvas.height -100) / 2,
     width: 10,
     height: 100,
     color: 'WHITE',
-    score: 0
+    score: 0,
+    speed: 5
 };
 
 //Evento de captura de tecla
@@ -71,7 +73,7 @@ function drawCircle(x, y, r, color){
     context.fillStyle = color;
     context.beginPath();
     //Método arc() Cria a curvatura da bola
-    context.arc(x, y, r, 0, 2*Math.PI);
+    context.arc(x, y, r, 0, 2*Math.PI, false);
     context.closePath();
     context.fill();
 }
@@ -104,11 +106,35 @@ function render(){
 function update(){
     //movimento da bola
     ball.x += ball.velocityX;
-    ball.y += ball.VelocityY;
+    ball.y += ball.velocityY;
+
+    //Move a raquete do usuario
+    user.y +=user.dy;
+
+    //Impede que a raquete do usuario saia do canvas
+    if(user.y < 0){
+        user.y = 0;
+    }else if(user.y +user.height > canvas.height){
+        user.y = canvas.height - user.height;
+    }
+
+    //Move a raquete do computador
+    if(ball.y < computer.y + computer.height/2){
+        computer.y -= computer.speed;
+    }else{
+        computer.y += computer.speed;
+    }
+
+    //Impede que o computador saia do jogo
+    if(computer.y < 0){
+        computer.y = 0;
+    }else if(computer.y + computer.height > canvas.height){
+        computer.y = canvas.height - computer.height;
+    }
 
     //detecção de colição com a corda superior e inferior
-    if(ball.y + ball.radius > canvas.height || ball.y - ball.canvas < 0){
-        ball.VelocityY = -ball.VelocityY;
+    if(ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0){
+        ball.velocityY = -ball.velocityY;
     }
 
     //detecta colição com a borda esquerda e direita
@@ -120,16 +146,17 @@ function update(){
         resetBall();
     }
 
-    //detecção com a raquete
+    //colisão com a raquete
     let player = (ball.x < canvas.width/2) ? user : computer
 
-    if(colision(ball, player)){
+    if(collision(ball, player)){
+        //inverte a direção da bola
         ball.velocityX = -ball.velocityX;
     }
 };
 
 //Sistema de Colisão
-function colision(b, p) {
+function collision(b, p) {
     p.top = p.y;
     p.bottom = p.y + p.height;
     p.left = p.x;
@@ -139,17 +166,19 @@ function colision(b, p) {
     b.bottom = b.y + b.radius;
     b.left = b.x - b.radius;
     b.right = b.x + b.radius;
-    return b.right > p.left && b.bottom > p.top && b.left < p.right && b.top < p.botoom;
+    return b.right > p.left && b.bottom > p.top && b.left < p.right && b.top < p.bottom;
 }
 
 function resetBall(){
-    ball.x = canvas.width/2
-    ball.y = canvas.hight/2
-    ball.velocityX = -ball.velocityX
+    ball.x = canvas.width/2;
+    ball.y = canvas.height/2;
+    ball.velocityX = -ball.velocityX;
+    ball.velocityY = 5 * (Math.random()> 0.5 ? 1 : -1);
     ball.speed = 7;
 }
 
 function game(){
+    update();
     render();
 }
 
